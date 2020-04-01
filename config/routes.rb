b6bb0dad4b7b63a1b52cf3dbe1842cac9532abb3
Andrea Tota <andrea.tota6@gmail.com>
2020-04-01 17:14:11 +0200

    App works all the way through edit with marking as done. Missing remove a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/destroy', to: ''
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
